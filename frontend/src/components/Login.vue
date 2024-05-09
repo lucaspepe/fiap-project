29a5248e98e100b7fe5135389cd3ae6905f27e68
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2024-05-09 20:02:32 -0300

    Update Login.vue

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,48 +1,82 @@
 <template>
-    <div class="login-content">
-        <div>
-            <img src="../assets/fiap-logo.svg" id="img-logo" alt="Logo fiap">
-        </div>
-        <form
-            id="app"
-            @submit="goToTarefas"
-            action=""
-            method="post"
-            >
-            <div class="group">
-                <input type="text" required>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label>Email</label>
+        <div class="login-content">
+            <div class="logo-img">
+                <img src="../assets/fiap-logo.svg" id="img-logo" alt="Logo fiap">
             </div>
-            <div class="group">
-                <input type="password" required>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label>Senha</label>
-            </div>
-            <!-- <button type="button" class="btn btn-login" @click="goToTarefas">Login</button>  -->
-            <input type="submit" class="btn btn-login" value="Login">
-        </form>
-
-        <!-- <div>
-            <input type="email" placeholder="example@verzel.com.br" name="Email" id="email" class="input-login">
+            <form id="app" @submit="login" action="" method="post" class="login-inputs">
+                <div class="input-container">
+                    <img src="../assets/mail.svg" alt="Icon email" class="icon-input">
+                    <div class="group">
+                        <input type="text" v-model="email" required>
+                        <p v-if="error.length">
+                        <p class="error-message">{{ error }}</p>
+                        </p>
+                        <p v-if="errorCredentials.length">
+                        <p class="error-message">{{ errorCredentials }}</p>
+                        </p>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                        <label>Email</label>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <img src="../assets/lock.svg" alt="Icon lock" class="icon-input">
+                    <div class="group">
+                        <input type="password" v-model="password" required>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                        <label>Senha</label>
+                    </div>
+                </div>
+                <input type="submit" class="btn btn-login" value="Login">
+            </form>
         </div>
-        <div>
-            <input type="password" placeholder="Senha" name="Email" id="password" class="input-login">
-        </div>
-        <button type="button" class="btn btn-login" @click="goToTarefas">Login</button> -->
-    </div>
-
 </template>
 <script>
 export default {
     name: "Login",
+    data() {
+        return {
+            error: '',
+            errorCredentials: '',
+            email: '',
+            password: ''
+        }
+    },
     methods: {
-        goToTarefas() {
-            console.log('heii')
-            this.$router.push("/tarefas");
+        async login(e) {
+            e.preventDefault();
+            this.validEmail()
+            if (!this.validEmail()) {
+                this.error = 'email deve conter @verzel.com.br';
+                return false
+            }
+            const data = {
+                email: this.email,
+                password: this.password
+            }
+
+            const dataJson = JSON.stringify(data)
+            const req = await fetch('http://localhost:8888/login', {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: dataJson
+            })
+           
+            if(req.status === 401) {
+                this.errorCredentials = 'Login invalido'
+                return false
+            }
+
+            const signIn = await req.json()
+            localStorage.setItem('token', signIn.token)
+            localStorage.setItem('user', signIn.user.email)
+            this.$router.push("/");
+            
         },
+        validEmail() {
+            return this.email.includes("@verzel.com.br") ? true : false
+        }
     },
 };
 </script>
@@ -61,6 +95,22 @@ button:hover {
 .input-login {
     margin-bottom: 1.5rem;
     outline: none;
+}
+
+.error-message {
+    margin: 0px;
+    padding: 0px;
+    color: red;
+    font-size: small;
+}
+
+.icon-input {
+    margin: 0px 15px 30px 0px;
+    width: 20px;
+}
+
+.input-container {
+    display: flex;
 }
 
 .login-content {
@@ -177,6 +227,26 @@ input:focus~.highlight {
     -webkit-animation: inputHighlighter 0.3s ease;
     -moz-animation: inputHighlighter 0.3s ease;
     animation: inputHighlighter 0.3s ease;
+}
+
+@media (min-width: 700px) {
+    .login-content {
+        display: flex;
+        align-items: center;
+    }   
+
+    #img-logo {
+        width: 300px;
+        margin: 0px;
+    }
+
+    .login-inputs {
+        border: 1px solid #ED1164;
+        padding: 40px;
+        border-radius: 30px;
+        margin-left: 5rem;
+    }
+
 }
 
 /* ANIMATIONS ================ */
