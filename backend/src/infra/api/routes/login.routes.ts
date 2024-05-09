5ce154f0ddbfe0c352264b7e0123b018ca33635a
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2024-05-09 19:43:53 -0300

    Update login.routes.ts

--- backend/src/infra/api/routes/login.routes.ts
+++ backend/src/infra/api/routes/login.routes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import AuthController from "../../../app/controllers/AuthController";
 
 export const loginRoute = express.Router();
