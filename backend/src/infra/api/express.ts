5ec424ef2897df1833149fba1e29e366abe03bef
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2024-05-09 19:44:19 -0300

    Update express.ts

--- backend/src/infra/api/express.ts
+++ backend/src/infra/api/express.ts
@@ -1,21 +1,25 @@
 import express, { Express } from "express";
+import cors from 'cors'
 import { Sequelize } from "sequelize-typescript";
-import UserModel from '../database/sequelize/models/user.model';
+import TarefaModel from '../database/sequelize/models/tarefa.model';
 import { loginRoute } from './routes/login.routes';
+import { tarefaRoute } from "./routes/tarefa.routes";
 
 export const app: Express = express();
+app.use(cors());
 app.use(express.json());
 app.use("/login", loginRoute);
+app.use("/tarefas", tarefaRoute);
 
 export let sequelize: Sequelize;
 
 async function setupDb() {
   sequelize = new Sequelize({
     dialect: "sqlite",
-    storage: ":memory:",
+    storage: "./database.sqlite",
     logging: false,
   });
-  sequelize.addModels([UserModel]);
+  sequelize.addModels([TarefaModel]);
   await sequelize.sync();
 }
 setupDb();
