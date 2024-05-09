6d190629f36e961edea739d719b2b008db6faf73
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2024-05-09 19:57:55 -0300

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,13 +3,26 @@ import Login from '@/components/Login.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
-    { path: '/', component: Login },
-    { path: '/tarefas', component: ListTarefas },
+    { path: '/', component: ListTarefas, meta: { requiresAuth: true } },
+    { path: '/login', component: Login },
 ]
 
 const router = createRouter({
     history: createMemoryHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+      const token = localStorage.getItem('token');
+      if (token) {
+        next();
+      } else {
+        next('/login');
+      }
+    } else {
+      next();
+    }
+  });
 
 export default router
